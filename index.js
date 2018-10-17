994513d191885466bf36b22be9bcded36f885e79
Dragan Bencic <dragan.bencic@gmail.com>
2018-10-17 14:58:23 +0200

    Imrpoved eval

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ function quoteIfString(literal) {
     }
 }
 
-function when() {
-    var options = arguments[arguments.length - 1];
-    console.log(options);
-    var expr = [];
-    for(var i=0; i<arguments.length - 1; i++) {
-        //1,3
-        expr.push((i % 2 == 0)?quoteIfString(arguments[i]):arguments[i]);
+function when(expression, options) {
+    if(arguments.length != 2) {
+        throw new Error("There should be only one expression enclosed in quotes (example {{#when 'a==b'}})");
     }
-    var result = eval(expr.join(" "));
+    var root = options.data.root;
+    var topcontainer = (global)?global:window;
+    Object.getOwnPropertyNames(root).forEach((function(name){
+        topcontainer[name] = root[name];
+    }));
+    // var options = arguments[arguments.length - 1];
+    // console.log(options);
+    // var expr = [];
+    // for(var i=0; i<arguments.length - 1; i++) {
+    //     //1,3
+    //     expr.push((i % 2 == 0)?quoteIfString(arguments[i]):arguments[i]);
+    // }
+    var result = eval(expression);
     if(result) {
         return options.fn(this);
     }else {
--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 var Handlebars = require('handlebars');
 var when = require("./index");
 var source = "<html><body>"
-    + "<p>{{#when mytext '==' 'sometext'}}text is sometext{{else}}text is NOT sometext, it is {{mytext}}{{/when}}</p>"
-    + "{{#when a '==' 1}}<p>A is 1</p>{{/when}}"
-    + "{{#when a '<=' 1}}<p>A is <= 1</p>{{/when}}"
-    + "<p>{{#when a '>' 1}}A is > 1{{else}}A is not > 1, it is {{a}}{{/when}}</p>"
-    + "<p>{{#when a '==' b}}A is EQUAL TO B{{else}}A is not equal to B. It is {{#when a '<=' b}}less or equal B{{else}}greater than b{{/when}}{{/when}}</p>"
-    + "<p>{{#when mytext '==' 'sometext'}}Again, mytext is sometext{{else when mytext '!=' 'sometext'}}mytext is NOT sometext checked inside else block{{/when}}</p>"
-    + "<p>{{#when a '==' b '||' a '==' 1}}a is equal b or is equal 1{{/when}}"
+    + "<p>{{#when 'a < b'}}a is less than b{{/when}}"
+    + "<p>{{#when 'a == b || (a == 1 && b > a)'}}a == b || (a == 1 and b > a){{/when}}"
+    // + "<p>{{#when mytext '==' 'sometext'}}text is sometext{{else}}text is NOT sometext, it is {{mytext}}{{/when}}</p>"
+    // + "{{#when a '==' 1}}<p>A is 1</p>{{/when}}"
+    // + "{{#when a '<=' 1}}<p>A is <= 1</p>{{/when}}"
+    // + "<p>{{#when a '>' 1}}A is > 1{{else}}A is not > 1, it is {{a}}{{/when}}</p>"
+    // + "<p>{{#when a '==' b}}A is EQUAL TO B{{else}}A is not equal to B. It is {{#when a '<=' b}}less or equal B{{else}}greater than b{{/when}}{{/when}}</p>"
+    // + "<p>{{#when mytext '==' 'sometext'}}Again, mytext is sometext{{else when mytext '!=' 'sometext'}}mytext is NOT sometext checked inside else block{{/when}}</p>"
+    // + "<p>{{#when a '==' b '||' a '==' 1}}a is equal b or is equal 1{{/when}}"
     + "</body></html>";
 
 var template = Handlebars.compile(source);
